52735a73762be3839592fe2ec1058dc96e3b871b
Degg254 <slpochta22@gmail.com>
2026-07-29 04:35:10 +0300

    Initial commit: Degg_Primitive_float - primitive float node compatible with Crystools CFloat

---
+++ degg_primitive_float.py
@@ -0,0 +1,37 @@
+import sys
+
+
+class DeggPrimitiveFloat:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "float": ("FLOAT", {
+                    "default": 1.0,
+                    "min": -sys.float_info.max,
+                    "max": sys.float_info.max,
+                    "step": 0.01,
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("FLOAT",)
+    RETURN_NAMES = ("float",)
+    FUNCTION = "process"
+    CATEGORY = "My_custom_nodes"
+    DESCRIPTION = "A primitive float node."
+
+    def process(self, float=1.0):
+        return (float,)
+
+
+NODE_CLASS_MAPPINGS = {
+    "DeggPrimitiveFloat": DeggPrimitiveFloat,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "DeggPrimitiveFloat": "Degg Primitive float",
+}
